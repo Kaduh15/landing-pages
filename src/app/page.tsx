0a33336f261e16fe772ae6f7b0c76e8b36de304a
kaduh15 <kadu.silva2014@gmail.com>
2024-02-21 14:01:58 -0300

    fix: error de build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import Details from './components/Details'
-import Header from './components/Header'
-import Hero from './components/Hero'
+import Details from '../components/Details'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
