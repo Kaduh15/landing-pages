3bb2cc9f391e3381610f188e08fa8c487916b4aa
kaduh15 <kadu.silva2014@gmail.com>
2024-02-22 13:23:01 -0300

    feat: cria o footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Benefits from '@/components/Benefits'
 import Depositions from '@/components/Depositions'
 import Details from '@/components/Details'
 import DownloadSession from '@/components/DownloadSession'
+import Footer from '@/components/footer'
 import Header from '@/components/Header'
 import HelpSession from '@/components/help'
 import Hero from '@/components/Hero'
@@ -16,6 +17,7 @@ export default function Home() {
       <Depositions />
       <DownloadSession />
       <HelpSession />
+      <Footer />
     </main>
   )
 }
---
+++ src/components/footer/index.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="bg-white text-center text-xs py-10">
+      <p>
+        Copyright © 2021 <strong>TeslaBank</strong>
+      </p>
+      <p>Todos os direitos Reservados</p>
+    </footer>
+  )
+}
