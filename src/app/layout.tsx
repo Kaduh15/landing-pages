ebe628cbf901147a883966167941f3543c047026
kaduh15 <kadu.silva2014@gmail.com>
2024-02-28 06:35:39 -0300

    feat: adiciona o analytics da vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react'
 import { twMerge } from 'tailwind-merge'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
@@ -18,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <Analytics />
       <body
         className={twMerge(
           inter.variable,
