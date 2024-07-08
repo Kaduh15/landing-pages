cffaefa679429a6dbc585c3e439ed413596254d7
Kadu <kadu.silva2014@gmail.com>
2024-07-08 07:03:55 -0300

    Fix: inicia com o tema salvo da ultima visita

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   const themeToggle = document.getElementById("theme-toggle");
   const themeIconContainer = document.getElementById("theme-icon-container");
-  const themeStore = window.localStorage.getItem("theme");
+  let themeStore = window.localStorage.getItem("theme");
 
   const loadSvg = (url) => {
     fetch(url)
@@ -18,10 +18,13 @@ document.addEventListener("DOMContentLoaded", function() {
   if (themeStore === "dark") {
     themeToggle.checked = true;
     document.body.classList.add("dark");
-    loadSvg("assets/icons/moon.svg");
+    loadSvg("assets/icons/sun.svg");
   }
   
   themeToggle.addEventListener("change", function() {
+    themeStore = this.checked ? "dark" : "light";
+    window.localStorage.setItem("theme", themeStore);
+
     if (this.checked) {
       loadSvg("assets/icons/moon.svg");
       document.body.classList.add("dark");
