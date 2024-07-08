88df1629057813ad8737fed1908244534f7ab163
Kadu <kadu.silva2014@gmail.com>
2024-07-08 07:36:06 -0300

    Feat:valida se a senha foi preenchida corretamente

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const themeToggle = document.getElementById("theme-toggle");
   const themeIconContainer = document.getElementById("theme-icon-container");
   let themeStore = window.localStorage.getItem("theme");
@@ -20,12 +20,12 @@ document.addEventListener("DOMContentLoaded", function() {
     document.body.classList.add("dark");
     loadSvg("assets/icons/sun.svg");
   }
-  
-  themeToggle.addEventListener("change", function() {
-    themeStore = this.checked ? "dark" : "light";
+
+  themeToggle.addEventListener("change", function () {
+    themeStore = themeToggle.checked ? "dark" : "light";
     window.localStorage.setItem("theme", themeStore);
 
-    if (this.checked) {
+    if (themeToggle.checked) {
       loadSvg("assets/icons/moon.svg");
       document.body.classList.add("dark");
     } else {
@@ -33,4 +33,33 @@ document.addEventListener("DOMContentLoaded", function() {
       document.body.classList.remove("dark");
     }
   });
+
+  validateForm();
 });
+
+function validateForm() {
+  const form = document.querySelector("#login-form");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const inputPassword = document.getElementById("password");
+
+    if (inputPassword.value.length < 8) {
+      const passwordInputDiv = document.querySelector('div > input[type="password"]').parentElement;
+
+      passwordInputDiv.insertAdjacentHTML('beforeend', '<p class="error-message">Senha deve ter no mínimo 8 caracteres</p>');
+
+      inputPassword.classList.add("input-error");
+
+      inputPassword.addEventListener('input', function (event) {
+        console.log(event.target.value);
+
+        if (event.target.value.length >= 8) {
+          inputPassword.classList.remove("input-error");
+          passwordInputDiv.querySelector('.error-message').remove()
+        }
+      })
+    }
+  });
+
+}
