44afdec9627e5df271cfe84063e60d289b31d6e6
Kadu <kadu.silva2014@gmail.com>
2024-07-08 06:50:37 -0300

    Feat: Adiciona botão de alterar tema

--- scripts.js
+++ scripts.js
@@ -1,15 +1,33 @@
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  // dark mode
-  const body = document.querySelector('body');
-  if (body) {
-    body.classList.add('dark');
-  }
-} else {
-  // light mode
-  const body = document.querySelector('body');
-  if (body) {
-    body.classList.remove('dark');
-  }
-}
+document.addEventListener("DOMContentLoaded", function() {
+  const themeToggle = document.getElementById("theme-toggle");
+  const themeIconContainer = document.getElementById("theme-icon-container");
+  const themeStore = window.localStorage.getItem("theme");
 
+  const loadSvg = (url) => {
+    fetch(url)
+      .then(response => response.text())
+      .then(svg => {
+        themeIconContainer.innerHTML = svg;
+      });
+  };
 
+  if (!themeStore) {
+    loadSvg("assets/icons/sun.svg");
+  }
+
+  if (themeStore === "dark") {
+    themeToggle.checked = true;
+    document.body.classList.add("dark");
+    loadSvg("assets/icons/moon.svg");
+  }
+  
+  themeToggle.addEventListener("change", function() {
+    if (this.checked) {
+      loadSvg("assets/icons/moon.svg");
+      document.body.classList.add("dark");
+    } else {
+      loadSvg("assets/icons/sun.svg");
+      document.body.classList.remove("dark");
+    }
+  });
+});
